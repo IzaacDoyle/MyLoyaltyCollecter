9aaffeaa6f7fbdb247014e7ac360350082d3b962
Izaac Doyle <34064518+IzaacDoyle@users.noreply.github.com>
2019-12-16 20:18:53 +0000

    Initial commit

---
+++ app/src/main/java/doyle/myloyaltycollecter/LoginActivity.kt
@@ -0,0 +1,12 @@
+package doyle.myloyaltycollecter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.loading_screen_start)
+    }
+}
